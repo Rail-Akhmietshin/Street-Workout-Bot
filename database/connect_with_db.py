00a98f4849502407493abf74b82f40dda3fbdac0
Rail <106035281+Rail-Akhmietshin@users.noreply.github.com>
2023-02-04 11:57:24 +0300

    Update connect_with_db.py

--- database/connect_with_db.py
+++ database/connect_with_db.py
@@ -8,7 +8,7 @@ class DataBase:
             database = str(os.environ.get("POSTGRES_DB")),
             user = str(os.environ.get("POSTGRES_USER")),
             password = str(os.environ.get("POSTGRES_PASSWORD")),
-            host = os.environ.get("POSTGRES_HOST"),
-            port = os.environ.get("POSTGRES_PORT")
+            host = "db",
+            port = os.environ.get("POSTGRES_PORT", default="5432")
         )
         return self.connection
